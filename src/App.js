d0cfb8a9a7a80129be9ef559f0a40fd2dbc3b32d
Peeyushmohapatra <peeyushm1998@gmail.com>
2023-02-19 21:26:52 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div style={{ display: "flex", flexDirection: "column", gap: "50px",textAlign:"center"}}>
-    <div style={{display:"flex",flexDirection:"column",gap:"10px"}}>
-      <p style={{fontSize:"55px",fontWeight:"bolder",fontFamily:"sans-serif"}}>The Evolution of</p>
-      <p style={{fontSize:"55px",fontWeight:"bolder",fontFamily:"sans-serif"}}>Airbnd's Frontend</p>
+      <div className="Manincontainer">
+    <div className="Firstdiv">
+      <p className="heading">The Evolution of</p>
+      <p className="heading">Airbnd's Frontend</p>
     </div>
     <div style={{display:"flex",flexDirection:"column",gap:"20px",justifyContent:"center",alignItems:"center"}}>
       <h4 style={{fontFamily:"sans-serif"}}>@ spikebrehm</h4>
       <img
         style={{ height: "50px", width: "60px" }}
         src="https://images.opencollective.com/airbnb/d327d66/logo.png"
+        alt=''
       ></img>
     </div>
   </div>
