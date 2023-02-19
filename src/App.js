6b584af676b4e6698bae645a920150c2d49ce80e
Peeyushmohapatra <peeyushm1998@gmail.com>
2023-02-19 21:13:52 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ display: "flex", flexDirection: "column", gap: "50px",textAlign:"center"}}>
+    <div style={{display:"flex",flexDirection:"column",gap:"10px"}}>
+      <p style={{fontSize:"55px",fontWeight:"bolder",fontFamily:"sans-serif"}}>The Evolution of</p>
+      <p style={{fontSize:"55px",fontWeight:"bolder",fontFamily:"sans-serif"}}>Airbnd's Frontend</p>
+    </div>
+    <div style={{display:"flex",flexDirection:"column",gap:"20px",justifyContent:"center",alignItems:"center"}}>
+      <h4 style={{fontFamily:"sans-serif"}}>@ spikebrehm</h4>
+      <img
+        style={{ height: "50px", width: "60px" }}
+        src="https://images.opencollective.com/airbnb/d327d66/logo.png"
+      ></img>
+    </div>
+  </div>
     </div>
   );
 }
